1a23e3755040580bc915ef7052064725fab5dcbc
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2023-11-15 21:22:36 +0100

    database connection added

--- src/domain/entities/ISocket.ts
+++ src/domain/entities/ISocket.ts
@@ -1,10 +1,15 @@
+import { Value } from '@libsql/client/.';
+
+/* eslint-disable prettier/prettier */
 export interface ServerToClientEvents {
+  'chat message': (msg: Value, result: string | undefined, username: Value) => void; //pasar a objeto
   noArg: () => void;
   basicEmit: (a: number, b: string, c: Buffer) => void;
   withAck: (d: string, callback: (e: number) => void) => void;
 }
 
 export interface ClientToServerEvents {
+  'chat message': (msg: string, username: string) => void;
   hello: () => void;
 }
 
--- src/infrastructure/server/index.ts
+++ src/infrastructure/server/index.ts
@@ -36,30 +36,55 @@ const db = createClient({
 db.execute(`
     CREATE TABLE IF NOT EXISTS messages (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      content TEXT
+      content TEXT,
+      username TEXT
     )
 `);
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   console.log('a user has connected');
 
   socket.on('disconnect', () => {
     console.log('a user has disconnected');
   });
 
-  socket.on('chat message', async (msg: object) => {
+  socket.on('chat message', async (msg: string) => {
+    console.log(`chat message => ${msg}`);
     let result;
+    const username = socket.handshake.auth.username ?? 'anonymous';
     try {
       result = await db.execute({
-        sql: 'INNSERT INTO messages (content) VALUES (:msg)',
+        sql: 'INSERT INTO messages VALUES (:msg, :username)',
         args: { msg }
       });
     } catch (error) {
       console.error(error);
       return;
     }
-    io.emit('chat message', msg, result.lastInsertRowid?.toString());
+    if (result.lastInsertRowid) {
+      io.emit('chat message', msg, result.lastInsertRowid.toString(), username); //habría que pasar esto a un objeto
+    }
   });
+
+  console.log('auth ⬇');
+  console.log(socket.handshake.auth);
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content, username FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      });
+
+      results.rows.forEach((row) => {
+        socket.emit('chat message', row.content, row.id?.toString());
+      });
+      console.log(results);
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+  }
 });
 
 app.use(logger('dev'));
